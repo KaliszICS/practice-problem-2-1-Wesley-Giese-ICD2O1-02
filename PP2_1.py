dfdf866ebf20a1e0e4294f3755581635823aa32c
Wesley-Giese-ICD2O1-02 <Wesley.giese@student.tdsb.on.ca>
2024-11-14 18:39:36 +0000

    j

--- PP2_1.py
+++ PP2_1.py
@@ -1,16 +1,29 @@
-
+'''
+    Lesson: 2.1
+    Author: Wesley Giese
+    Date Created: October 9, 2024
+    Date Last Modified: November 14, 2024
+'''
 
 def q1(): 
   #Write Assignment code here
-
-
+  num1 = int(input("In: ")) 
+  if num1 % 2 ==0:
+    print(f"{num1} is even")
+  if num1 % 2 ==1:
+    print(f"{num1} is odd")
 def q2(): 
   #Write Assignment code here
-
+  name1 = str(input("In: "))
+  name1 == "Kalisz"
+  if name1 == "Kalisz":
+    print("teacher")
+  if name1 != "Kalisz":
+    print("student")
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
+# q1()
+# q2()
